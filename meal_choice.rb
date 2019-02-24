c31387c5c3e74bede7f8d63d9ff65a9667f81e0f
Matthew Muldrow <jmatthewm92@gmail.com>
2019-02-24 10:34:18 +0000

    Done.

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,4 @@
-# Define a method meal_choice that returns the meal_choice that was
-# passed into it and defaults to meat.
+def meal_choice(a="meat") do
+  puts meal_choice 
+  expect(meal_choice("meat")).to eq("meat")
+end
